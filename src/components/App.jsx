0a7a02c0f465c86d587bfd6b1aa5d228905e02d4
Ishita Bansal <ishitabansal770@gmail.com>
2024-03-17 01:15:41 +0530

    added navbar

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./nav/Nav";
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
